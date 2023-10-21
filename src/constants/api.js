884f7ff971fba5ba3583d80eaa31e1493b043a7e
Md Moin <developermdmoin@gmail.com>
2023-10-21 15:25:09 +0530

    api added

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,7 +1,8 @@
 
 import axios from 'axios'
 
-export const API = axios.create({ baseURL: 'http://localhost:2000/api/v1' })
+export const API = axios.create({ baseURL: 'https://ozonbiotech.onrender.com' })
+// export const API = axios.create({ baseURL: 'http://localhost:2000/api/v1' })
 // API.defaults.headers.common['Content-Type'] = 'application/json'
-  
+
 
