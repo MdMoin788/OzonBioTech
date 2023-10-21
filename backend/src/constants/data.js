5fc0579f19c6c434dccbbe8f05391e5bc5266316
Md Moin <developermdmoin@gmail.com>
2023-10-21 14:46:29 +0530

    client

--- backend/src/constants/data.js
+++
@@ -1,8 +0,0 @@
-
-export const whiteListedUrl = [
-  'http://localhost:3000',
-  'http://localhost:3001',
-  'http://localhost:8080',
-]
-
-
--- backend/src/utils/handleOrderController.js
+++
@@ -1,8 +0,0 @@
-import { Product } from "../models/productModel.js"
-
-
-export const updateStocks = async(productId, productQuantity) => {
-    const product = await Product.findById(productId)
-    product.stock = product.stock - productQuantity
-    await product.save({ validateBeforeSave: false })
-}
